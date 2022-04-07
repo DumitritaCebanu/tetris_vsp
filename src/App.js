ce58dff4159a7421fd8c3f7ab064ad689ef77964
DumitritaCebanu <dumitrita.cebanu.99@gmail,com>
2022-04-07 18:53:51 +0300

    4:User-can-see-background-with-squares

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+
     <div className="container">
         <div className="board">
+            <div className="item"></div>
         </div>
     </div>
   );
