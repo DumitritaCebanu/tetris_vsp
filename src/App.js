55dce90f8362bf3d9065cbf9e46d01f80e624cc6
DumitritaCebanu <dumitrita.cebanu.99@gmail,com>
2022-05-04 18:45:04 +0300

    8:new tetromino appear and detect collision

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import './App.css';
-import Tetrominos from './Tetrominos.jsx';
+import React, { useState, useCallback } from "react";
+
+import { GameContainer } from "./components/GameContainer";
+import { RightPanel } from "./components/RightPanel";
+import { TileBoard } from "./components/TitleBoard";
+import { useGameTime } from "./hooks/useGameTime";
+import { useBoard } from "./hooks/useBoard";
+import "./App.css";
+
 function App() {
 
+    const [speed, setSpeed] = useState(1000);
 
+    const  [updateBoard, board] = useBoard();
 
-  return (
-    <div className="container">
-        <div className="board">
-            <Tetrominos/>
-            <div className="item"></div>
-        </div>
-    </div>
-  );
+    const onTick = useCallback(() => {
+        console.log("tic tic");
+        updateBoard();
+    }, []);
+
+    const { isRunning, startTime, stopTime } = useGameTime({ onTick, speed });
+
+    return (
+        <GameContainer>
+            <TileBoard board={board} />
+            <RightPanel>
+                <button onClick={startTime} disabled={isRunning}>
+                    START
+                </button>
+                <button onClick={stopTime} disabled={!isRunning}>
+                    STOP
+                </button>
+                <button onClick={() => setSpeed((prev) => prev - 100)}>
+                    GO FASTER
+                </button>
+                <span>time is {isRunning ? "curge" : "not curge"}</span>
+            </RightPanel>
+        </GameContainer>
+    );
 }
 
 export default App;
